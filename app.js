36288f93df8beab69f07642bec7ed7716dc5172f
Floresda <97864447+Floresda@users.noreply.github.com>
2022-04-19 14:45:50 -0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,19 @@
+let Header= document.createElement("h1");
+let textParagraph= document.createElement("p");
+let list = document.createElement("p");
+let list2 = document.createElement("p");
+let list3 = document.createElement("p");
+
+Header.textContent="Indiana Spring Time Flowers"
+textParagraph.textContent = "Springtime in Indiana during the month of April consist of rain, sun, and occasional snow believe or not. This period of time trees and flowers will start to bloom and show their colors after a cold winter.";
+list.textContent = "* Shooting Star";
+list2.textContent = "* Virginia Waterleaf";
+list3.textContent = "* Fire Pink";
+    // flower list: shooting star,virginia waterleaf, fire pink
+
+
+document.body.appendChild(Header);
+document.body.appendChild(textParagraph);
+document.body.appendChild(list);
+document.body.appendChild(list2);
+document.body.appendChild(list3);
